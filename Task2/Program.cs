a0d939d76c0fabb7b4539e6cc1032fce7b9890f2
Matvey Kolomov <matvey.kolomov134@gmail.com>
2024-02-07 22:32:15 +1000

    Sem:Task2:Functiones

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив из N случайных чисел (N вводится с клавиатуры)
+// Найти кол-во чисел, которые оканчиваются на 1 и деляться нацело на 7
+// Пример [1 5 11 21 81 4 0 91 2 3] => 2
+
+int[] GetArrayFromConsole()
+{
+    System.Console.Write("Size: ");
+    int size = int.Parse(Console.ReadLine()!);
+    int[] array = new int[size];
+
+    System.Console.WriteLine($"Enter {size} elements");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int[] GetRandomArrayFromConsole()
+{
+    
+}
